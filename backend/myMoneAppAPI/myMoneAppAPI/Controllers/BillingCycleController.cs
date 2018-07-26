983131d90c5d0faa68d76ecaef03668fdcc6d6f0
Fagner Gomesfag <fagner.egomes@hotmail.com>
2018-07-26 13:14:22 -0300

    Configuramos Autenticação e Autorization na WebApi

--- backend/myMoneAppAPI/myMoneAppAPI/Controllers/BillingCycleController.cs
+++ backend/myMoneAppAPI/myMoneAppAPI/Controllers/BillingCycleController.cs
@@ -10,7 +10,7 @@ using System.Web.Http.Results;
 
 namespace myMoneAppAPI.Controllers
 {
-    [EnableCors(origins: "http://localhost:8080", headers: "*", methods: "*")]
+    [Authorize]
     public class BillingCycleController : ApiController
     {
         static readonly IBillingCycleRepositorio billingRepositorio = new BillingCycleRepositorio();
---
+++ backend/myMoneAppAPI/myMoneAppAPI/Services/myMoneySeguranca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myMoneAppAPI.Services
+{
+    public class myMoneySeguranca
+    {
+        public static bool Login(string login, string senha)
+        {
+            var usuario = new
+            {
+                login = "fagner.gomes",
+                senha = "alfa4721"
+            };
+
+            if (usuario.login == login && usuario.senha == senha)
+                return true;
+            else
+                return false;
+        }
+    }
+}
