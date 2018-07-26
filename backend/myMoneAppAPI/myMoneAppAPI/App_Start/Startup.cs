13bb5fcb3cc4ebb8f6564268819e8e7ab71e8a2b
Fagner Gomesfag <fagner.egomes@hotmail.com>
2018-07-26 10:54:33 -0300

    Atualização dos pacotes da WebApi

---
+++ backend/myMoneAppAPI/myMoneAppAPI/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(myMoneAppAPI.App_Start.Startup))]
+
+namespace myMoneAppAPI.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}", defaults: new { id = RouteParameter.Optional });
+            app.UseWebApi(config);
+        }
+    }
+}
